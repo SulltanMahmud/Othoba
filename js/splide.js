1dcfe53b1ed4a029a538eef8ed9382569f7e5d3a
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-01-26 17:34:31 +0600

    add

--- js/splide.js
+++ js/splide.js
@@ -14,3 +14,34 @@
    perPage: 4,
  });
  splide2.mount();
+
+ const hotDealsSplide = new Splide("#product", {
+   arrows: false,
+   pagination: false,
+   type: "loop",
+   perPage: 2,
+   autoplay: true,
+   interval: 2000,
+   breakpoints: {
+     640: {
+       height: "6rem",
+     },
+   },
+ });
+ hotDealsSplide.mount();
+
+
+ const desert = new Splide("#desert", {
+   arrows: false,
+   pagination: false,
+   type: "loop",
+   perPage: 4,
+   autoplay: true,
+   interval: 2000,
+   breakpoints: {
+     640: {
+       height: "6rem",
+     },
+   },
+ });
+ desert.mount();
